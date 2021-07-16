e4f5987ed6a08e075b2d223e98161812a98477f1
Codophilus Persicus <mmirzanejad2@gmail.com>
2021-07-16 13:28:46 +0430

    Create name.c

---
+++ name.c
@@ -0,0 +1,14 @@
+//C code to repeat a name 100 times,
+
+
+int main() 
+{ 
+char name[]="MyName"; 
+int i, n=1; 
+ 
+  for(i=0;i<100;i++) 
+    { 
+    printf("%d %s\n",n,name); 
+    n++; 
+    } 
+}
